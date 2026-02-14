964e0f709bce35abb787dd3959dee368c0804110
David Cornejo <koerdev@gmail.com>
2026-02-13 22:31:48 -0300

    feat: print variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
+// Se recomienda dejar las variables constantes fuera de la función
+const firstName = 'David';
+const lastName = 'Cornejo';
+
+const favouriteGames = ['Rocket League', 'TFT', 'Life is Strange', 'GTA: San Andreas'];
+const isActive = false;
+
+const address = {
+  zipCode: 'ABC-123',
+  country: 'Chile',
+};
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>David</h1>
-      <h3>Cornejo</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favouriteGames.join(', ')}</p>
+      <p>{2 + 2}</p>
+
+      <h3>{isActive ? 'Activo' : 'No Activo'}</h3>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   );
 }
